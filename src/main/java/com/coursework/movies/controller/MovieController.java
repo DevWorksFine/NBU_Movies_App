3f76c4fffa6308ef33a6a11d47e063981745f96f
Недялко Ангелов <nedialkoangelov62@gmail.com>
2020-07-23 20:57:27 +0300

    login other stuff :wq

--- src/main/java/com/coursework/movies/controller/MovieController.java
+++ src/main/java/com/coursework/movies/controller/MovieController.java
@@ -9,11 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 import java.util.List;
 
@@ -48,6 +47,5 @@ public class MovieController extends AbstractController {
 
         return this.getView("movie/movieDetails",model);
     }
-
 
 }
